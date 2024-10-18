65e61295e7c6e40b895dba50220cf2bdfd072148
Hamzajaved96 <hamzajaved9696@gmail.com>
2024-10-18 21:51:47 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcAppWithCode.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
